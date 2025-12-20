e53ad131695405d95ddf973053069bb72b99dbea
yosdani <ytabaressilverio86@gmail.com>
2025-12-20 03:29:18 -0500

    fix: add migrations

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::apiResource('posts', \App\Http\Controllers\PostController::class);
